b5a3325377059241cd4deccb84b6eb56e47cebb4
Hocker AGI <contacto.hocker@gmail.com>
2026-02-03 04:01:33 -0800

    Crear public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Cache mínimo (no rompe tu app)
+self.addEventListener("fetch", () => {});
