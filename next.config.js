58025886998740571ffed1475ae703b769765525
Hocker AGI <contacto.hocker@gmail.com>
2026-02-07 01:28:10 -0800

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb"
+    }
+  }
 };
-module.exports = nextConfig;
+
+export default nextConfig;
