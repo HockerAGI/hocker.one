d1e4a4310b27afed4f36bdf8849fc7d33679ad81
Hocker AGI <contacto.hocker@gmail.com>
2026-03-27 14:26:59 -0700

    Actualizar sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,8 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
 });
-
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-
 // Cache mínimo (no rompe tu app)
 self.addEventListener("fetch", () => {});
