1c2407eccfa72c08f4514a13ba82bc696543de42
Hocker AGI <contacto.hocker@gmail.com>
2026-04-03 16:50:29 -0700

    Actualizar VoiceInput.tsx

--- src/components/VoiceInput.tsx
+++ src/components/VoiceInput.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 
-type SpeechRecognitionType = typeof window extends undefined
+type SpeechRecognitionType = Window;
   ? never
   : typeof window & {
       SpeechRecognition?: new () => SpeechRecognition;
