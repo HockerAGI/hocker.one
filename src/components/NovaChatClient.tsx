fa216d60f0121d662d18d6e4955298796a1689d3
Hocker AGI <contacto.hocker@gmail.com>
2026-04-04 12:28:45 -0700

    Actualizar NovaChatClient.tsx

--- src/components/NovaChatClient.tsx
+++ src/components/NovaChatClient.tsx
@@ -1,10 +1,7 @@
-"use client";
+'use client';
 
 import NovaChat from "@/components/NovaChat";
-import { useWorkspace } from "@/components/WorkspaceContext";
 
 export default function NovaChatClient() {
-  const { projectId } = useWorkspace();
-
-  return <NovaChat projectId={projectId} />;
+  return <NovaChat />;
 }
