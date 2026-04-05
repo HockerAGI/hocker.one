b1b04fbea4306cae9edcd661dc52d14179c477ed
Hocker AGI <contacto.hocker@gmail.com>
2026-04-05 01:41:55 -0700

    Actualizar CommandBoxClient.tsx

--- src/components/CommandBoxClient.tsx
+++ src/components/CommandBoxClient.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import CommandBox from "@/components/CommandBox";
-import { useWorkspace } from "@/components/WorkspaceContext";
 
 export default function CommandBoxClient() {
-  const { projectId, nodeId } = useWorkspace();
-
-  return <CommandBox projectId={projectId} nodeId={nodeId} />;
+  return <CommandBox />;
 }
